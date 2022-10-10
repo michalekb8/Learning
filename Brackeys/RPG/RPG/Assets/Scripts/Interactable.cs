d8bafb4513a31adbb14a0738a3cc68741494caf5
Michal <michalekb8@gmail.com>
2022-10-10 22:13:38 +0200

    Youtube - Brackeys - RPG

---
+++ Brackeys/RPG/RPG/Assets/Scripts/Interactable.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+
+    public float radius = 3f;
+    public Transform interactionTransform;
+
+    bool isFocused = false;
+    Transform player;
+
+    bool hasInteracted = false;
+
+    public virtual void Interact()
+    {
+        Debug.Log("Interacting with " + transform.name);
+    }
+
+    private void Update()
+    {
+        if (isFocused && !hasInteracted)
+        {
+            float distance = Vector3.Distance(player.position, interactionTransform.position);
+            if (distance <= radius)
+            {
+                Interact();
+                hasInteracted = true;
+            }
+        }
+    }
+    public void OnFocused(Transform playerTransform)
+    {
+        isFocused = true;
+        player = playerTransform;
+        hasInteracted = false;
+    }
+
+    public void OnDefcused()
+    {
+        isFocused = false;
+        player = null;
+        hasInteracted = false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (interactionTransform == null)
+        {
+            interactionTransform = transform;
+        }
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(interactionTransform.position, radius);
+    }
+
+}
