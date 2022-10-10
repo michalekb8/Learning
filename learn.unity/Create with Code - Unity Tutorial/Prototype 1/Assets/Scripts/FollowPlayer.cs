26c9927ad72a9d9ab445e50ad71351e98cf4f070
Michal <michalekb8@gmail.com>
2022-10-10 22:15:11 +0200

    Learn.unity - CWC - Prototype 1

---
+++ learn.unity/Create with Code - Unity Tutorial/Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset1 = new Vector3(0, 5, -7);
+    private Vector3 offset2 = new Vector3(0, 2, 1.75f);
+    private int camera = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+            camera *= -1;
+
+
+        if (camera == 1)
+        {
+            //Offset the camera behind the player by adding to the player's position
+            transform.position = player.transform.position + offset1;
+        }
+        if (camera == -1)
+        {
+            //Offset the camera perspective of the driver’s seat.
+            transform.position = player.transform.position + offset2;
+        }
+    }
+
+
+}
